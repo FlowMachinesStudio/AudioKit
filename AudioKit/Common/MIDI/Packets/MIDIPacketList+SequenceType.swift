dc1604da8761dad00ad92f0853f26ec46ef44e6f
Shimoyama <shimoyama.daily@gmail.com>
2023-12-07 17:13:06 +0900

    fix crash

--- AudioKit/Common/MIDI/Packets/MIDIPacketList+SequenceType.swift
+++ AudioKit/Common/MIDI/Packets/MIDIPacketList+SequenceType.swift
@@ -15,18 +15,60 @@ extension MIDIPacketList: Sequence {
     }
 
     public func makeIterator() -> AnyIterator<Element> {
-        var p: MIDIPacket = packet
-        var idx: UInt32 = 0
 
-        return AnyIterator {
-            guard idx < self.numPackets else {
-                return nil
-            }
-            defer {
-                p = MIDIPacketNext(&p).pointee
+        withUnsafePointer(to: packet) { ptr in
+            var p = ptr
+            var idx: UInt32 = 0
+
+            return AnyIterator {
+                guard idx < self.numPackets else {
+                    return nil
+                }
+
                 idx += 1
+                let packet = extractPacket(p)
+                p = UnsafePointer(MIDIPacketNext(p))
+                return packet
             }
-            return p
         }
+    }
+    
+    public func extractPacketData(_ ptr: UnsafePointer<MIDIPacket>) -> [UInt8] {
+
+        let raw = UnsafeRawPointer(ptr)
+        let dataPtr = raw.advanced(by: MemoryLayout.offset(of: \MIDIPacket.data)!)
+
+        let length = Int(raw.loadUnaligned(fromByteOffset: MemoryLayout.offset(of: \MIDIPacket.length)!,
+                                           as: UInt16.self))
+
+        var array = [UInt8](repeating: 0, count: length)
+        memcpy(&array, dataPtr, length)
+
+        return array
+    }
+    
+    public func extractPacket(_ ptr: UnsafePointer<MIDIPacket>) -> MIDIPacket? {
+
+        var packet = MIDIPacket()
+        let raw = UnsafeRawPointer(ptr)
+
+        let length = raw.loadUnaligned(fromByteOffset: MemoryLayout.offset(of: \MIDIPacket.length)!,
+                                       as: UInt16.self)
+
+        // We can't represent a longer packet as a MIDIPacket value.
+        if length > 256 {
+            return nil
+        }
+
+        packet.length = length
+        packet.timeStamp = raw.loadUnaligned(fromByteOffset: MemoryLayout.offset(of: \MIDIPacket.timeStamp)!,
+                                             as: MIDITimeStamp.self)
+
+        let dataPtr = raw.advanced(by: MemoryLayout.offset(of: \MIDIPacket.data)!)
+        _ = withUnsafeMutableBytes(of: &packet.data) { ptr in
+            memcpy(ptr.baseAddress!, dataPtr, Int(length))
+        }
+
+        return packet
     }
 }
